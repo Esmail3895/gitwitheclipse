c414f07d5692ae962ad9ec4e2ece3defa8901553
Esmail3895 <esmailpaltasingh123@gmail.com>
2021-09-12 17:41:46 +0530

    AddDemo.java created and code written inside it.

---
+++ EcliseWithGit/src/com/esmail/AddDemo.java
@@ -0,0 +1,15 @@
+package com.esmail;
+
+public class AddDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Sum of two num: "+add(2, 2));
+
+	}
+
+	private static int add(int i, int j) {
+		return i+j;
+		
+	}
+
+}
