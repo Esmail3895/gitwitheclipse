9ce9b686b65aae48997036c9213f2ee2f6271c6d
Esmail3895 <esmailpaltasingh123@gmail.com>
2021-09-12 17:45:36 +0530

    Sub method added

--- EcliseWithGit/src/com/esmail/AddDemo.java
+++ EcliseWithGit/src/com/esmail/AddDemo.java
@@ -5,12 +5,19 @@ public class AddDemo {
 	public static void main(String[] args) {
 		//method call
 		System.out.println("Sum of two num: "+add(2, 2));
+		System.out.println("Substracton of two num: "+sub(2, 2));
 
 	}
+
 //addition of two no in the method
 	private static int add(int i, int j) {
 		return i+j;
 		
 	}
+	private static int sub(int i, int j) {
+		// TODO Auto-generated method stub
+		return i-j;
+	}
+	
 
 }
